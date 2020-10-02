c7fb77967ac5ca871bb20cffa8463dca64eaad2e
Gonestel <www.ikfgbr@gmail.com>
2020-10-03 00:37:51 +0300

    Добавьте файлы проекта.

---
+++ USRS1/Линейная скорость искусственного спутника Земли.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace USRS1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите массу [кг]");
+            string mass = Console.ReadLine();
+            double m = Convert.ToDouble(mass);
+            Console.WriteLine("Введите радиус планеты [км]");
+            string rad = Console.ReadLine();
+            double r = Convert.ToDouble(rad);
+            Console.WriteLine("Введите высоту [км]");
+            string heig = Console.ReadLine();
+            double h = Convert.ToDouble(heig);
+            double g = 0.0098, s = 2, v;
+            v = Math.Sqrt((g * Math.Pow(r, s)) / (r + h));
+            Console.WriteLine("Результат = {0} км/с", v);
+            Console.ReadLine();
+
+        }
+    }
+}
